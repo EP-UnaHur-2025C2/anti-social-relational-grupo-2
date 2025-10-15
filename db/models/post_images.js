27f84447bf10d2a95f3b8d2089db04408aa16bb1
dylan <dylancotera@gmail.com>
2025-10-15 14:55:20 -0300

    creo modelos y modifico index

---
+++ db/models/post_images.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post_Images extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Post_Images.init({
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Post_Images',
+  });
+  return Post_Images;
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("UnaHur - Anti-Social net");
+const express = require("express")
+const app = express()
+const db = require("../db/models")
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.listen(PORT, async () => {
+    await db.sequelize.sync()
+    console.log(`El servidor esta corriendo en el puerto ${PORT}`)
+})
